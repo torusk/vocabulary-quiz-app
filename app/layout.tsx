6ff4d0edfe4feecd1223a91da5e76818b131d4c1
torusk <shancun.kazuki@gmail.com>
2025-03-28 14:42:18 +0900

    ファビコン設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
+  icons: {
+    icon: '/E.png',
+    apple: '/ei.png',
+  },
 }
 
 export default function RootLayout({
